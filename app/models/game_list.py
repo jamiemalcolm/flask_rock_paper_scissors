145e342bf72d345069823c4626cae61f08876738
jamiemalcolm <jmalcolm281@gmail.com>
2020-12-04 16:49:16 +0000

    RPS logic complete

--- app/models/game_list.py
+++ app/models/game_list.py
@@ -1,5 +1,19 @@
 from app.models.game_class import Game
+from app.models.player_class import Player
+
+jamie = Player("Jamie", "rock")
+laura = Player("Laura", "scissors")
+
+game_1 = Game(jamie, laura)
+
+
+
 
 def compare(choice_1, choice_2):
-    if choice_1 == "scissors":
-        return choice_1
+    if choice_1 == "rock" and choice_2 == "scissors":
+        return f"{choice_1} wins!"
+    if choice_1 == "paper" and choice_2 == "rock":
+        return f"{choice_1} wins!"
+    if choice_1 == "scissors" and choice_2 == "paper":
+        return f"{choice_1} wins!"
+    return f"{choice_2} wins!"
